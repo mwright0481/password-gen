74d799d8134e49c6603e1a6ecf1f4e248ce77ade
mwright0481 <95373969+mwright0481@users.noreply.github.com>
2022-02-02 10:30:19 -0500

    Add files via upload

--- style.js
+++ style.js
@@ -1,162 +1,123 @@
-var cityResultText = $("#cityResult");
-var tempResultText = $("#tempResult");
-var humidityResult = $("#humidityResult");
-var windResultText = $("#windResult");
-var mainIcon =$("#mainIcon");
-var rowCards = $("#rowCards");
-var dayForecast = $("#row5day");
-var cardDisplay = $("#cardDisplay");
-var UVIndexText = $("#UVIndexResult");
-var buttonList = $("#buttonsList");
-var forecastDate = {};
-var forecastIcon = {};
-var forecastTemp = {};
-var forecastHum = {};
-var today = moment().format('DD' + "/" + 'MM' + '/' + 'YYYY');
-var APIKey = "&units=metric&APPID=123babda3bc150d180af748af99ad173";
-var url =  "https://api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}";
-var citiesArray = JSON.parse(localStorage.getItem("Saved City")) || [];
+var generateBtn = document.querySelector("#generate");
+var passwordLength;
+var confirmLower;
+var confirmUpper;
+var confirmNumber;
+var confirmSpecial;
+var userChoices;
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var blankUpper = [];
+var toUpper = function (x) {
+  return x.toUpperCase();
+};
+upperCase = lowerCase.map(toUpper);
 
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 
-$(document).ready(function (){
-    var userInput = citiesArray[citiesArray.length - 1];
-    currentWeather(userInput);
-    forecast(userInput);
-    lastSearch ();
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-});
-
-function currentWeather(userInput) {
-    mainIcon.empty();
-    var queryURL = url + userInput + APIKey;
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response){
-        var cityInfo = response.name;
-        var country = response.sys.country; 
-        var temp = response.main.temp;
-        var humidity = response.main.humidity;
-        var wind = response.wind.speed;
-        var lat = response.coord.lat;
-        var lon = response.coord.lon;
-        var icon = response.weather[0].icon;
-        var UVindexURL = "https://api.openweathermap.org/data/2.5/uvi?" + "lat=" + lat + "&" + "lon=" + lon + "&APPID=123babda3bc150d180af748af99ad173";
-        var newImgMain = $("<img>").attr("class", "card-img-top").attr("src", "https://openweathermap.org/img/wn/" + icon + "@2x.png");
-        mainIcon.append(newImgMain);
-        cityResultText.text(cityInfo + ", " + country + " " + today);
-        tempResultText.text("Temperature: " + temp + " ºC");
-        humidityResult.text("Humidity: " + humidity + " %");
-        windResultText.text("Wind Speed: " + wind + " MPH");
-        $.ajax({
-            url: UVindexURL,
-            method: "GET"
-        }).then(function(uvIndex){
-            var UV = uvIndex.value;
-            var colorUV;
-            if (UV <= 3) {
-                colorUV = "green";
-            } else if (UV >= 3 & UV <= 6) {
-                colorUV = "yellow";
-            } else if (UV >= 6 & UV <= 8) {
-                colorUV = "orange";
-            } else {
-                colorUV = "red";
-            }
-            UVIndexText.empty();
-            var UVResultText = $("<p>").attr("class", "card-text").text("UV Index: ");
-            UVResultText.append($("<span>").attr("class", "uvindex").attr("style", ("background-color: " + colorUV)).text(UV))
-            UVIndexText.append(UVResultText);
-            cardDisplay.attr("style", "display: flex; width: 98%");
-        })    
-    })
-    }
-
-function forecast (userInput) {
-    dayForecast.empty();
-    rowCards.empty();
-    var fore5 = $("<h2>").attr("class", "forecast").text("5-Day Forecast: "); 
-    var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&units=metric&APPID=123babda3bc150d180af748af99ad173";
-    $.ajax({
-        url: forecastURL,
-        method: "GET"
-    }).then(function(response){
-        for (var i = 0; i < response.list.length; i += 8){
-            
-            forecastDate[i] = response.list[i].dt_txt;
-            forecastIcon[i] = response.list[i].weather[0].icon;
-            forecastTemp[i] = response.list[i].main.temp; 
-            forecastHum[i] = response.list[i].main.humidity;  
-
-            var newCol2 = $("<div>").attr("class", "col-2");
-            rowCards.append(newCol2);
-
-            var newDivCard = $("<div>").attr("class", "card text-white bg-primary mb-3");
-            newDivCard.attr("style", "max-width: 18rem;")
-            newCol2.append(newDivCard);
-
-            var newCardBody = $("<div>").attr("class", "card-body");
-            newDivCard.append(newCardBody);
-
-            var newH5 = $("<h5>").attr("class", "card-title").text(moment(forecastDate[i]).format("MMM Do"));
-            newCardBody.append(newH5);
-
-            var newImg = $("<img>").attr("class", "card-img-top").attr("src", "https://openweathermap.org/img/wn/" + forecastIcon[i] + "@2x.png");
-            newCardBody.append(newImg);
-
-            var newPTemp = $("<p>").attr("class", "card-text").text("Temp: " + Math.floor(forecastTemp[i]) + "ºC");
-            newCardBody.append(newPTemp);
-
-            var newPHum = $("<p>").attr("class", "card-text").text("Humidity: " + forecastHum[i] + " %");
-            newCardBody.append(newPHum);
-
-            dayForecast.append(fore5);
-            };
-            })
-
-        }
-
-function storeData (userInput) {
-    var userInput = $("#searchInput").val().trim().toLowerCase();
-    var containsCity = false;
-
-    if (citiesArray != null) {
-        $(citiesArray).each(function(x) {
-			if (citiesArray[x] === userInput) {
-				containsCity = true;
-			}
-		});
-	}
-	if (containsCity === false) {
-        citiesArray.push(userInput);
-	}
-	localStorage.setItem("Saved City", JSON.stringify(citiesArray));
+  // passwordText.value = password;
 }
-function lastSearch () {
-    buttonList.empty()
-    for (var i = 0; i < citiesArray.length; i ++) {
-        var newButton = $("<button>").attr("type", "button").attr("class","savedBtn btn btn-secondary btn-lg btn-block");
-        newButton.attr("data-name", citiesArray[i])
-        newButton.text(citiesArray[i]);
-        buttonList.prepend(newButton);
-    }
-    $(".savedBtn").on("click", function(event){
-        event.preventDefault();
-        var userInput = $(this).data("name");
-        currentWeather(userInput);
-        forecast(userInput);
-    })
-}
-$(".btn").on("click", function (event){
-    event.preventDefault();
-    if ($("#searchInput").val() === "") {
-    alert("Please type a userInput to know the current weather");
-    } else
-    var userInput = $("#searchInput").val().trim().toLowerCase();
-    currentWeather(userInput);
-    forecast(userInput);
-    storeData();
-    lastSearch();
-    $("#searchInput").val("");
+generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+  passwordLength = prompt("How many characters would you like your password? Choose between 8 and 128");
+  console.log("Password length " + passwordLength);
+  
+  if(!passwordLength) {
+    alert("Required value");
 
-})
+  } 
+  else if (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = prompt("You must choose between 8 and 128");
+    console.log("Password length " + passwordLength);
+  
+  } 
+  else { 
+    confirmLower = confirm("Will this contain lower case letters?");
+    console.log("Lower case " + confirmLower);
+    confirmUpper = confirm("Will this contain upper case letters?");
+    console.log("Upper case " + confirmUpper);
+    confirmNumber = confirm("Will this contain numbers?");
+    console.log("Number " + confirmNumber);
+    confirmSpecial = confirm("Will this contain special characters?");
+    console.log("Special Character " + confirmSpecial);
+
+  };
+    if (!confirmLower && !confirmUpper && !confirmNumber && !confirmSpecial) {
+    userChoices = alert("You must choose a criteria");
+     } 
+     else if (confirmLower && confirmUpper && confirmNumber && confirmSpecial) {
+        userChoices = lowerCase.concat(upperCase, numbers, special);
+        console.log(userChoices);
+      }
+    
+      else if (confirmLower && confirmUpper && confirmNumber) {
+        userChoices = lowerCase.concat(upperCase, numbers);
+        console.log(userChoices);
+      }
+      else if (confirmLower && confirmUpper && confirmSpecial) {
+        userChoices = lowerCase.concat(upperCase, special);
+        console.log(userChoices);
+      }
+      else if (confirmLower && confirmNumber && confirmSpecial) {
+        userChoices = lowerCase.concat(numbers, special);
+        console.log(userChoices);
+      }
+      else if (confirmUpper && confirmNumber && confirmSpecial) {
+        userChoices = upperCase.concat(numbers, special);
+        console.log(userChoices);
+      }
+      else if (confirmLower && confirmUpper) {
+        userChoices = lowerCase.concat(upperCase);
+        console.log(userChoices);
+      }
+      else if (confirmLower && confirmNumber) {
+        userChoices = lowerCase.concat(numbers);
+        console.log(userChoices);
+      }
+      else if (confirmLower && confirmSpecial) {
+        userChoices = lowerCase.concat(special);
+        console.log(userChoices);
+      }
+      else if (confirmUpper && confirmNumber) {
+        userChoices = upperCase.concat(numbers);
+        console.log(userChoices);
+      }
+      else if (confirmUpper && confirmSpecial) {
+        userChoices = upperCase.concat(special);
+        console.log(userChoices);
+      }
+      else if (confirmNumber && confirmSpecial) {
+        userChoices = numbers.concat(special);
+        console.log(userChoices);
+      }
+      else if (confirmLower) {
+        userChoices = lowerCase;
+        console.log(userChoices);
+      }
+      else if (confirmUpper) {
+        userChoices = blankUpper.concat(upperCase);
+        console.log(userChoices);
+      }
+      else if (confirmNumber) {
+        userChoices = numbers;
+        console.log(userChoices);
+      }
+      else if (confirmSpecial) {
+        userChoices = special;
+        console.log(userChoices);
+      }
+      var passwordBlank = [];
+      for (var i = 0; i < passwordLength; i++) {
+        var allChoices = userChoices[Math.floor(Math.random() * userChoices.length)];
+        passwordBlank.push(allChoices);
+        console.log(allChoices);
+      }
+      var password = passwordBlank.join("");
+      console.log("Your Pasword is: " + password);
+      return password;
+      
+    }
